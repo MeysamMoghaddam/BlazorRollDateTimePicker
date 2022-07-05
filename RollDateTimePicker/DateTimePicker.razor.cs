8ca39da75012a87916f17d5862b56b52052a7e44
MeysamMoghaddam <65253484+MeysamMoghaddam@users.noreply.github.com>
2022-07-05 13:04:14 +0430

    Export DateTime

--- RollDateTimePicker/DateTimePicker.razor.cs
+++ RollDateTimePicker/DateTimePicker.razor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -13,10 +14,18 @@ namespace RollDateTimePicker
         [Parameter] public string Identifier { get; set; }
         [Parameter] public string PlaceHolder { get; set; }
         /// <summary>
+        /// Gets the culture name in the format languagecode2-country/regioncode2.
+        /// example: en-US
+        /// </summary>
+        [Parameter] public string CultureInfoName { get; set; } = "en-US";
+        /// <summary>
         /// 2017-10-21 23:52:50
         /// </summary>
         [Parameter] public string Value { get; set; }
         [Parameter] public EventCallback<string> ValueChanged { get; set; }
+
+        [Parameter] public DateTime MValue { get; set; }
+        [Parameter] public EventCallback<DateTime> MValueChanged { get; set; }
 
         [Parameter] public DateTimePickerOption Options { get; set; }
         private DotNetObjectReference<DateTimePicker>? dotNetHelper;
@@ -33,8 +42,13 @@ namespace RollDateTimePicker
             }
         }
         [JSInvokable]
-        public async void Confirm(string Date) => await ValueChanged.InvokeAsync(Date);     
+        public async void Confirm(string Date)
+        {
+            await ValueChanged.InvokeAsync(Date);
+            var pcul = new CultureInfo(CultureInfoName);
+            if(DateTime.TryParseExact(Date, Options.DateFormat, pcul,DateTimeStyles.None,out var mValue))            
+                await MValueChanged.InvokeAsync(mValue);            
+            
+        }
     }
-
-
 }
